a59e244d67930a28faf88f6debb743cb39ce1930
“mintic03” <narensilva@gmail.com>
2022-09-28 21:52:44 -0500

    clinica5

--- clinica/settings.py
+++ clinica/settings.py
@@ -25,8 +25,10 @@ SECRET_KEY = 'django-insecure-sc_5bn6g@45=9mjz$j=0rwc074=h!#-ytr&8quk^dvsf5%eeft
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -37,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'clinicaAPI'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
